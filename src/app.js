40ba91cc53ec38518f27635443cf638145485349
LuisoDev1 <luisbet92@gmail.com>
2025-11-22 00:15:03 +0000

    rescatando codigo js perdido

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const arrayPalos = ["♦","♥","♠","♣"];
+  const arrayNumeros = ["A",2,3,4,5,6,7,8,9,"J","Q","K"];
+
+  //FUNSION PARA GENERAR PALOS ALEATORIOS
+  const generadorAleatorio = (array) => {
+
+    let paloAleatorio = Math.floor(Math.random() * array.length);
+    return array[paloAleatorio];
+  }
+
+  let obtenerPalo = generadorAleatorio(arrayPalos);
+  let paloSuperior = document.querySelector(".palo-superior");
+  let paloInferior = document.querySelector(".palo-inferior");
+  if(obtenerPalo == "♦" || obtenerPalo == "♥") {
+    paloSuperior.style.color = "red";
+    paloInferior.style.color = "red";
+  }
+  paloSuperior.textContent = obtenerPalo;
+  paloInferior.textContent = obtenerPalo;
+
+
+   let obtenerNumero = generadorAleatorio(arrayNumeros);
+   let cuerpoNumero = document.querySelector(".cuerpo-numero");
+   cuerpoNumero.textContent = obtenerNumero;
+
 };
+
+
